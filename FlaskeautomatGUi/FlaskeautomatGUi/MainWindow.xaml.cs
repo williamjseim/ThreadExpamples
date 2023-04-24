9fae4eeef25f1bc93c49d7f22d586c75b5ae3529
williamjseim <33179905+williamjseim@users.noreply.github.com>
2023-04-24 13:31:57 +0200

    asd

---
+++ FlaskeautomatGUi/FlaskeautomatGUi/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAnimatedGif;
+
+namespace FlaskeautomatGUi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        void test()
+        {
+                box.RenderTransform = new TranslateTransform(box.RenderTransformOrigin.X, box.RenderTransformOrigin.Y + 5);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            asd.RenderTransform = new TranslateTransform(asd.RenderTransform.Value.OffsetX+100, asd.RenderTransform.Value.OffsetY);
+
+        }
+    }
+}
