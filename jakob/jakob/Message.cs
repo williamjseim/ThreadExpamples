b712cfe96afb0a0691368638dceff3f6ca7fbdb4
williamjseim <33179905+williamjseim@users.noreply.github.com>
2023-05-10 12:40:21 +0200

    asd

---
+++ jakob/jakob/Message.cs
@@ -0,0 +1,24 @@
+﻿
+namespace jakob
+{
+    public enum MessageCarrier { Smtp, VMessage }
+    internal class Message
+    {
+        string to, from, body, subject, cc;
+
+        public Message(string to, string from, string body, string subject, string cc)
+        {
+            this.to = to;
+            this.from = from;
+            this.body = body;
+            this.subject = subject;
+            this.cc = cc;
+        }
+
+        public string To { get { return to; } set { to = value; } }
+        public string From { get { return from; } set { from = value; } }
+        public string Body { get { return body; } set { body = value; } }
+        public string Subject { get { return subject; } set { subject = value; } }
+        public string Cc { get { return cc; } set { cc = value; } }
+    }
+}
---
+++ jakob/jakob/MessageSender.cs
@@ -0,0 +1,23 @@
+﻿
+namespace jakob
+{
+    static internal class MessageSender
+    {
+        static public void sendMessageToAll(string[] to, Message m, bool isHTML)
+        {
+            if (isHTML)
+                m.Body = ConvertBodyToHTML(m.Body);
+        }
+
+        static public void sendMessage(Message m, bool isHTML)
+        {
+            if (isHTML)
+                m.Body = ConvertBodyToHTML(m.Body);
+        }
+
+        static public string ConvertBodyToHTML(string plainText)
+        {
+            return "" + plainText + "";
+        }
+    }
+}
