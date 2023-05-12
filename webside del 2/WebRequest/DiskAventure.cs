390388fb4f5f07eabb641fb04a5f48a9a89d912d
williamjseim <33179905+williamjseim@users.noreply.github.com>
2023-05-12 10:05:15 +0200

    asd

---
+++ webside del 2/WebRequest/DiskAventure.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebRequest
+{
+    internal class DiskAventure : IGetResponse
+    {
+        public string GetResponse(string url)
+        {
+            return GetFile(url).Result;
+        }
+
+        private async Task<string> GetFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                return await File.ReadAllTextAsync(path);
+            }
+            return "sut";
+        }
+    }
+}
---
+++ webside del 2/WebRequest/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace WebRequest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DiskAventure adventure = new DiskAventure();
+            string s = Console.ReadLine();
+            Console.WriteLine(adventure.GetResponse(s));
+        }
+    }
+}
